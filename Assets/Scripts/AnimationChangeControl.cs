2e97b1a995230c3dd92c0fb3beed96dcd98c3d89
89trillion-chenkai <chenkai@89trillion.com>
2021-07-02 20:24:24 +0800

    完成动画切换设置

---
+++ Assets/Scripts/AnimationChangeControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationChangeControl : MonoBehaviour
+{
+    private Animator animator;
+    private KeyCode lastButton;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if (!Input.GetKeyDown(lastButton)) //如果和上次按键不同才进入
+        {
+            if (Input.GetKeyDown(KeyCode.A)) //攻击
+            {
+                animator.SetBool("attackTrigger", true);
+                lastButton = KeyCode.A;
+            }
+            else if (Input.GetKeyDown(KeyCode.R)) //跑步
+            {
+                animator.SetBool("runTrigger", true);
+                lastButton = KeyCode.R;
+            }
+            else if (Input.GetKeyDown(KeyCode.I)) //闲置
+            {
+                animator.SetBool("waitTrigger", true);
+                lastButton = KeyCode.I;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Game04
+
+## 1.整体框架
+
+使用Unity引擎的Animator组件控制动画的切换
+
+## 2.目录结构
+```
+｜——Scripts   
+｜  ｜——AniamtionChange  
+｜  ｜  ｜——AnimationChangeControl     #数据管理       
+｜    
+```
+## 3.代码逻辑
+
+通过获取键盘上的按键判断该改变哪个动画切换参数，动画参数为Trigger类型。
+
+## 4.存储设计
+
+士兵模型预制体
